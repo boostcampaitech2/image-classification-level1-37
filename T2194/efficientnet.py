14ed4907b32013cd454738bed6a4abe4c9c9753f
jae <manatom55@gmail.com>
2021-08-27 23:46:13 +0900

    efficientnet training

--- T2194/efficientnet.py
+++ T2194/efficientnet.py
@@ -56,14 +56,7 @@ loaders = DataLoader(
     DATA,batch_size=batch_size,shuffle=True,num_workers=1)
 loss_function = loss_fn(DATA)
 
-efficientnet = timm.create_model('tf_efficientnet_b0', pretrained=True,num_classes=18)
-#for param in efficientnet.parameters():
-#    param.requires_grad = False unfreeze하니까 조금 더 잘 나왔어요
-
-efficientnet.conv_head = nn.Sequential(nn.Conv2d(320, 640, kernel_size=(3, 3), stride=(1, 1), bias=False),
-                            nn.BatchNorm2d(640, eps=0.001, momentum=0.1, affine=True, track_running_stats=True),
-                            nn.SiLU(inplace=True),nn.Dropout(p=0.3),nn.Conv2d(640, 1280, kernel_size=(1, 1), stride=(1, 1), bias=False))    
-efficientnet.classifier = nn.Linear(in_features=1280, out_features=18, bias=True)
+efficientnet = myefficientnet_b0()
 
 if torch.cuda.is_available():
     efficientnet.cuda()
