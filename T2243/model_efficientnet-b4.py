b660f5a0f7398e63f87810ae61c60c77db054b12
홍석진_T2243 <59007816+HongCu@users.noreply.github.com>
2021-08-29 23:27:01 +0900

    modify efficientnet-b4 model (dropout)

--- T2243/model_efficientnet-b4.py
+++ T2243/model_efficientnet-b4.py
@@ -6,7 +6,8 @@ class MyModel(nn.Module) :
         self.model_name = EfficientNet.from_pretrained('efficientnet-b4', 
                                                 in_channels=3, 
                                                 num_classes=18) # weight가져오고 num_classes(두번째 파라미터로 학습시키는 class 수)
-    
+        self.dropout = torch.nn.Dropout(p=0.5)
     def forward(self, x) :
         x = F.relu(self.model_name(x))
+        x = self.dropout(x)
         return x
