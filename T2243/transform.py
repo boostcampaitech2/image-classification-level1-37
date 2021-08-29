15c168ce123b1ed69059d75f711a4f31338fe0a8
홍석진_T2243 <59007816+HongCu@users.noreply.github.com>
2021-08-29 23:32:07 +0900

    modify transform mean, std (from baseline code)

--- T2243/transform.py
+++ T2243/transform.py
@@ -4,13 +4,12 @@ train_transform = T.Compose([
     T.RandomHorizontalFlip(0.5),
     T.RandomRotation(10),
     T.ToTensor(),
-    T.Normalize(mean=(0.56, 0.51, 0.48), std=(0.22, 0.24, 0.25))
+    T.Normalize(mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246))
 ])
 
 valid_transform = T.Compose([
     T.ToPILImage(),
     T.CenterCrop([300,250]),
     T.ToTensor(),
-    T.Normalize(mean=(0.56, 0.51, 0.48), std=(0.22, 0.24, 0.25))
+    T.Normalize(mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246))
 ])
-
